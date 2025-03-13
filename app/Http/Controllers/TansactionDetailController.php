2d3a3b54fe3437ec8d2660f5c4e7e5a534f7326b
kusnii <mkhusni377@gmail.com>
2025-03-13 15:18:49 +0700

    Migration

---
+++ app/Http/Controllers/TansactionDetailController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TansactionDetail;
+use Illuminate\Http\Request;
+
+class TansactionDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TansactionDetail $tansactionDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TansactionDetail $tansactionDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TansactionDetail $tansactionDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TansactionDetail $tansactionDetail)
+    {
+        //
+    }
+}
